d82a262ced2d7e997b8b99e6612c342941703d16
Kenneth Caple <kennethcaple@Kenneths-iMac-Pro.local>
2023-11-20 08:40:46 -0700

    ca

--- modules/custom/collaborative_audio/src/Plugin/Field/FieldFormatter/WaveformFormatter.php
+++ modules/custom/collaborative_audio/src/Plugin/Field/FieldFormatter/WaveformFormatter.php
@@ -1,3 +1,34 @@
 <?php
 
-namespace Drupal
+namespace Drupal\collaborative_audio\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'waveform' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "waveform",
+ *   label = @Translation("Audio Waveform"),
+ *   field_types = {
+ *     "file"
+ *   }
+ * )
+ */
+class WaveformFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      // Render each item as waveform.
+    }
+
+    return $elements;
+  }
+}
