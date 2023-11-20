9c479aff4450682f93d95ce33ba134111d358c77
Kenneth Caple <kennethcaple@Kenneths-iMac-Pro.local>
2023-11-20 09:11:40 -0700

    ca

--- modules/custom/collaborative_audio/js/collaborative_audio.js
+++ modules/custom/collaborative_audio/js/collaborative_audio.js
@@ -1,7 +1,19 @@
-(function ($, Drupal) {
-  Drupal.behaviors.collaborativeAudioBehavior = {
-    attach: function (context, settings) {
-      // Initialize WaveSurfer.js here
+(function ($) {
+  Drupal.behaviors.collaborativeAudioWaveform = {
+    attach: function (context) {
+    $('.field--name-field-audio-file', context).once('collaborativeAudioWaveform').each(function () {
+
+        var audioFileUrl = $(this).find('audio').attr('src');
+        var waveSurfer = WaveSurfer.create({
+          container: this,
+          waveColor: 'violet',
+          progressColor: 'purple'
+        });
+
+        if (audioFileUrl) {
+          waveSurfer.load(audioFileUrl);
+        }
+      });
     }
   };
-})(jQuery, Drupal);
+})(jQuery);
