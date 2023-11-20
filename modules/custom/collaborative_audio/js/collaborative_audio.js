16711e9e72156cc56b719a26f1b4d55a75fa0959
Kenneth Caple <kennethcaple@Kenneths-iMac-Pro.local>
2023-11-20 08:32:57 -0700

    added collaborative audio module

---
+++ modules/custom/collaborative_audio/js/collaborative_audio.js
@@ -0,0 +1,7 @@
+(function ($, Drupal) {
+  Drupal.behaviors.collaborativeAudioBehavior = {
+    attach: function (context, settings) {
+      // Initialize WaveSurfer.js here
+    }
+  };
+})(jQuery, Drupal);
